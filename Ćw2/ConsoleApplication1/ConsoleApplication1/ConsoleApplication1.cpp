1a0b0906199e87894fc65e527d60983ab7e759ce
ZagajXxX <115549642+ZagajXxX@users.noreply.github.com>
2022-10-28 10:09:48 +0200

    Add files via upload

---
+++ Ćw2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "Wpisz cenę produktu:" << endl;
+	cin >> a;
+	cout << "Wpisz cenę sprzedaży:" << endl;
+	cin >> b;
+	int c = b - a;
+		if (c > 0) {
+			cout << "Zysk:" << c;
+		}
+		if (c < 0) {
+			cout << "Strata:" << -c;
+		}
+		if (c == 0) {
+            cout << "Brak zysku i straty.";
+		}
+}
