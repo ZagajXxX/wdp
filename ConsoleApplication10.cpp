1059a03e79c5ea2805a2c604d6fbdb9409cede51
ZagajXxX <115549642+ZagajXxX@users.noreply.github.com>
2022-12-02 11:11:48 +0100

    Add files via upload

---
+++ ConsoleApplication10.cpp
@@ -0,0 +1,37 @@
+﻿// ConsoleApplication10.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+void randomnumbers() {
+    
+    int result = 0, count5 = 0, count1 = 0;
+    srand(time(NULL));
+    while (count5 < 3 or count1 < 2) {
+        result = rand() % 11 - 5;
+        if (result == 5) {
+            count5++;
+            cout << result << " ";
+        }
+        else if (result == 1)
+        {
+            count1++;
+                cout << result << " ";
+        }
+     }
+}
+int main()
+{
+    randomnumbers();
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
