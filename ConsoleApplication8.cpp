c9f57e803d7440fec71b9ed3c8373d748163b3a5
ZagajXxX <115549642+ZagajXxX@users.noreply.github.com>
2022-12-02 10:57:03 +0100

    Add files via upload

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,46 @@
+﻿// ConsoleApplication8.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int second(int arg[], int lenght) {
+	int largest, secondlargest;
+	if (arg[1] > arg[0]) {
+		largest = arg[1];
+		secondlargest = arg[0];
+	}
+	else { 
+		largest = arg[0];
+		secondlargest = arg[1]; }
+	for (int j = 2; j < lenght; j++)
+	{
+		if (largest < arg[j])
+		{
+			secondlargest = largest;
+			largest = arg[j];
+		}
+		else if (arg[j] > secondlargest && arg[j] < largest)
+		{
+			secondlargest = arg[j];
+		}
+
+	}
+		return secondlargest;
+}
+
+int main()
+{
+	int arr[20];
+	srand(time(NULL));
+
+	int lenght = sizeof(arr) / sizeof(int);
+
+	for (int i = 0; i < size(arr); i++)
+	{
+		arr[i] = rand() % 101;
+		cout << arr[i] << " ";
+	}
+	cout << second(arr, lenght) << endl;
+}
+
---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,24 @@
+﻿// ConsoleApplication9.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+string reversedText(string a){
+	string reversedresult = a;
+	for (int i = 0; i < a.length(); i++)
+	{
+		reversedresult[i] = a[a.length() - 1 - i];
+	}
+	return reversedresult;
+}
+
+int main()
+{
+	string napis;
+	cout << "enter some string" << endl;
+	cin >> napis;
+	cout << reversedText(napis);
+}
+
+
